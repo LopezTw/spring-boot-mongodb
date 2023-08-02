cea616437b80e5c44904afabb65488e219b03be1
LopezTw <caiohg_94@hotmail.com>
2023-08-02 14:56:06 -0300

    Atualizaçao de usuario com PUT

--- src/main/java/com/lopez/mongo/domain/resources/UserResource.java
+++ src/main/java/com/lopez/mongo/domain/resources/UserResource.java
@@ -54,4 +54,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT) 
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){  
+		User obj = service.fromDTO(objDto); // Converte o DTO para USER
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
 }
