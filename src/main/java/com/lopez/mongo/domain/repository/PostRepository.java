3290565394b2cfaaeac06bf865cd143af1ac1048
LopezTw <caiohg_94@hotmail.com>
2023-08-02 20:23:40 -0300

    Consulta simples com query methods

--- src/main/java/com/lopez/mongo/domain/repository/PostRepository.java
+++ src/main/java/com/lopez/mongo/domain/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.lopez.mongo.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,11 +10,8 @@ import com.lopez.mongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	
+	List<Post> findByTitleContainingIgnoreCase(String text); // Consulta criada, ignore case vai ignorar maiuscula e minuscula
 	
 }
 
 
-/*
- *  Essa interface vazia desse jeito, ja consegue fazer varias operacoes basicas (salvar, deletar, recuperar...)
- */
--- src/main/java/com/lopez/mongo/domain/resources/PostResource.java
+++ src/main/java/com/lopez/mongo/domain/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.lopez.mongo.domain.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lopez.mongo.domain.Post;
+import com.lopez.mongo.domain.resources.util.URL;
 import com.lopez.mongo.domain.services.PostService;
 
 @RestController  // Essa anotation define que essa classe eh um recurso REST
@@ -24,5 +28,14 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj); 
 	}
 	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET) 
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue ="") String text){ 
+		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		
+		
+		return ResponseEntity.ok().body(list); 
+	}
 	
 }
