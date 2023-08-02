d55998c913450afa7a3e7361578c10d5d88c4fed
LopezTw <caiohg_94@hotmail.com>
2023-08-02 11:54:58 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/lopez/mongo/domain/resources/UserResource.java
+++ src/main/java/com/lopez/mongo/domain/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.lopez.mongo.domain.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lopez.mongo.domain.User;
 import com.lopez.mongo.domain.services.UserService;
+import com.lopez.mongo.dto.UserDTO;
 
 @RestController  // Essa anotation define que essa classe eh um recurso REST
 @RequestMapping(value = "/users") // caminho do endpoint definido
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET) // Pra dizer que esse metodo vai ser o caminho endpoint de findall, precisamos definir o metodo GET atraves do RequestMapping
-	public ResponseEntity<List<User>> findAll(){ // ResponseEntity vai servir pra ENCAPSULAR toda uma estrutura necessária p/ retornarmos respostas HTTP já com possiveis cabeçalhos, erros...
+	public ResponseEntity<List<UserDTO>> findAll(){ // ResponseEntity vai servir pra ENCAPSULAR toda uma estrutura necessária p/ retornarmos respostas HTTP já com possiveis cabeçalhos, erros...
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list); // ok é um metodo que vai instanciar o ResponseEntity ja com o codigo de resposta HTTP de que ocorreu com sucesso. E o body eh o corpo da resposta.
+		List<UserDTO> listDto = list.stream().map(usuario -> new UserDTO(usuario)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto); // ok é um metodo que vai instanciar o ResponseEntity ja com o codigo de resposta HTTP de que ocorreu com sucesso. E o body eh o corpo da resposta.
 	}
 	
 }
