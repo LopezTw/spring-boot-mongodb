2d52b5ef45b3ee9a202d53e38d5a0e87013cd149
LopezTw <caiohg_94@hotmail.com>
2023-08-02 16:02:44 -0300

    Projeçao dos dados do autor com DTO

--- src/main/java/com/lopez/mongo/config/Instantiation.java
+++ src/main/java/com/lopez/mongo/config/Instantiation.java
@@ -12,6 +12,7 @@ import com.lopez.mongo.domain.Post;
 import com.lopez.mongo.domain.User;
 import com.lopez.mongo.domain.repository.PostRepository;
 import com.lopez.mongo.domain.repository.UserRepository;
+import com.lopez.mongo.dto.AuthorDTO;
 
 /**
  * Esta classe serve basicamente pra fazermos as cargas iniciais do banco de dados, para realizarmos os testes
@@ -39,10 +40,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
