64b289e61cbe8da5702fc31e28584a33f4f8d411
LopezTw <caiohg_94@hotmail.com>
2023-08-02 20:45:58 -0300

    Consulta simples com @Query

--- src/main/java/com/lopez/mongo/domain/repository/PostRepository.java
+++ src/main/java/com/lopez/mongo/domain/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.lopez.mongo.domain.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.lopez.mongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } } ")
+	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); // Consulta criada, ignore case vai ignorar maiuscula e minuscula
 	
